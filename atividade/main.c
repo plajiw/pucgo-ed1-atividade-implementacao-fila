5b1a76a4bfdedd7cfffa24d6ce2b0a97cb86c60b
plajiw <pabloribeiro.cont@gmail.com>
2026-03-04 22:48:09 -0300

    ajustar main

--- atividade/main.c
+++ atividade/main.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 #include "queue.h"
 
-int main()
-{
+int main() {
     Queue q;
 
+    printf("Iniciando a Fila\n");
     initQueue(&q);
+
+    printf("Enfileirando: 1, 2, 3\n");
     enqueue(&q, 1);
     enqueue(&q, 2);
     enqueue(&q, 3);
+    
+    printf("Estado atual da fila: ");
+    printQueue(&q);
 
-    firstElement(&q);
+    int primeiro = firstElement(&q);
+    printf("\nPrimeiro elemento: %d\n", primeiro);
 
+    printf("Executar dequeue\n");
     dequeue(&q);
 
-    firstElement(&q);
+    primeiro = firstElement(&q);
+    printf("Novo primeiro elemento após dequeue: %d\n", primeiro);
 
+    printf("Estado atual da fila: ");
+    printQueue(&q);
+
+    printf("\nEnfileirando: 4, 5, 6\n");
     enqueue(&q, 4);
     enqueue(&q, 5);
     enqueue(&q, 6);
 
-    indexElement(&q, 3);
-
+    printf("Estado atual da fila: ");
     printQueue(&q);
 
-    destroyQueue(&q);
+    int valorBusca = 3;
+    int indice = indexElement(&q, valorBusca);
+
+    printf("\nO elemento [%d] esta na posicao (indice): %d\n", valorBusca, indice);
+    printf("\nO elemento [%d] nao foi encontrado.\n", valorBusca);
+
+    printf("Limpar fila\n");
+    eraseQueue(&q);
+    
+    printf("Fila finalizada com sucesso.\n");
+
+    getchar();
+    return 0;
 }
